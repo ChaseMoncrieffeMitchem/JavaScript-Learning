6a54d0fec9a2812801dbc9db9e2c3fabc1b61d73
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-02-12 23:00:09 -0600

    daily JS practice

--- adv.js
+++ adv.js
@@ -22,3 +22,15 @@ function sortLowToHigh(numbers) {
 }
 
 console.log(sortLowToHigh([1, 5, 0, 203, 235, 645, 1]))
+
+function sortHighToLow () {
+    return numbers.sort((a, b) => b.price - a.price)
+}
+
+console.log(
+    sortHighToLow([
+        { id: 1, price: 50},
+        { id: 2, price: 0},
+        { id: 3, price: -500},
+    ])
+)
