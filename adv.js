03bc3d0d53aab262983ada3f9331fc0bb1eac504
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-02-14 15:50:10 -0600

    Daily JS practice

--- adv.js
+++ adv.js
@@ -42,3 +42,19 @@ async function firstSixIncomplete(usersId) {
 }
 
 console.log(firstSixImcomplete(6))
+
+function showRating(rating) {
+    let rating = ""
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratings += "*"
+        if (i !== Math.floor(rating) - 1) {
+            ratings =+ " "
+        }
+    }
+    if (!Number.isInteger(rating)) {
+        ratings += " ."
+    }
+    return ratings;
+}
+
+console.log(showRating(3.5))
