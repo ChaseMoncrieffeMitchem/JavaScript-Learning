c30281d10ebb1e35de090a8df456f6af63ff51e0
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-03-03 11:15:48 -0600

    JS Practice

--- adv.js
+++ adv.js
@@ -158,3 +158,18 @@ const pyramid = (n) => {
         console.log(step)
     }
 }
+
+const steps = (n) => {
+    for (let row = 0; row < n; ++row) {
+        let step = "";
+        for (let column = 0; column < n; ++column) {
+            if (column <= row) {
+                step += "#"
+            }
+            else {
+                step += " ";
+            }
+        }
+        console.log(step)
+    }
+}
